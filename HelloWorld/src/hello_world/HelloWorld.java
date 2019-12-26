3e67cdb348f8f578a686cc648b6cf02d44a4a190
sfianchao1022 <sfianchao@yahoo.com.tw>
2019-12-26 23:57:14 +0800

    java

---
+++ HelloWorld/src/hello_world/HelloWorld.java
@@ -0,0 +1,8 @@
+package hello_world;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("my first java code");
+	}
+}
