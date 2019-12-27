c1070eb96e4a56155de38c41594b87fcc8d0c338
sfianchao1022 <58282058+sfianchao1022@users.noreply.github.com>
2019-12-28 02:52:57 +0800

    java

--- HelloWorld/src/hello_world/HelloWorld.java
+++ HelloWorld/src/hello_world/HelloWorld.java
@@ -4,5 +4,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("my first java code");
+		
+		String name="Ian";
+	    int age=24;
+	    double desiredSalary=1000000;
+	    char gender='m';
+	    char c='!';
+	    boolean lookingForJob=false;
+	    
+	    System.out.println("this is "+name+c);
+	    System.out.println("my lucky number is "+(3+4));//7
 	}
 }
