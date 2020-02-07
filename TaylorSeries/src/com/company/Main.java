e37af6f83688f9bae088e737d778c257c93cbed3
sfianchao1022 <sfianchao@yahoo.com.tw>
2020-02-07 23:27:47 +0800

    .

--- TaylorSeries/src/com/company/Main.java
+++ TaylorSeries/src/com/company/Main.java
@@ -2,34 +2,31 @@ package com.company;
 
 public class Main {
 
-    public final static int e=3;
+    //public final static int e=3;
 
     public static void main(String[] args) {
 	// write your code here
-        //x + x^2/2! + x^3/3! + ...
-        
+        //1 + x + x^2/2! + x^3/3! + ...
+
         //e=3;
-        System.out.println(calculate(e));
+        System.out.println(calculate(4,15));
 
         //System.out.println(hierarchy(5));
     }
 
-    public static double calculate(int x){
+    public static double calculate(int e,int n){
 
-        if(x==0){
-            return 0;
+        if(n==0){
+            return 1;
         }
         else{
-            //System.out.println(Math.pow(2,x));
-            //System.out.println(hierarchy(x));
-            //System.out.println(e);
-            return (Math.pow(e,x)/(double)hierarchy(x))+calculate(x-1);
+            return (Math.pow(e,n)/hierarchy(n)+calculate(e,n-1));
 
         }
 
     }
 
-    public static int hierarchy(int a){
+    public static double hierarchy(int a){
         if(a==1){
             return 1;
         }
