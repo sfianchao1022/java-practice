7d6dc8c3730567478a5d1c910bdfaebe171bf678
sfianchao1022 <sfianchao@yahoo.com.tw>
2020-02-22 02:15:37 +0800

    Pow

---
+++ Pow/src/ExampleTest.java
@@ -0,0 +1,29 @@
+public class ExampleTest {
+
+    public static void main(String[] args){
+        Double output=myPow(2.00000,-2);
+        System.out.println(output);
+    }
+
+    public static double myPow(double x, int n) {
+        if (n<0){
+
+            if (n==0){
+                return 1;
+            }else{
+//            n=n-1;
+                return (1/x)*myPow(x,++n);
+            }
+
+        }else{
+
+            if (n==0){
+                return 1;
+            }else{
+//            n=n-1;
+                return x*myPow(x,--n);
+            }
+
+        }
+    }
+}
